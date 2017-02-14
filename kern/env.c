1923d7d64d0b06bf8a66d0f2b5a963cdcb7316db
Endymion <495609715@qq.com>
2017-02-14 23:37:40 +0800

    Part A of Lab3 finished

--- kern/env.c
+++ kern/env.c
@@ -521,11 +521,14 @@ env_run(struct Env *e)
 	//	e->env_tf to sensible values.
 
 	// LAB 3: Your code here.
-	if (curenv) curenv->env_status = ENV_RUNNABLE;
-	curenv = e;
-	curenv->env_status = ENV_RUNNING;
-	curenv->env_runs++;
-	lcr3(PADDR(curenv->env_pgdir));
+	if (curenv != e){
+		if (curenv)
+			curenv->env_status = ENV_RUNNABLE;
+		curenv = e;
+		curenv->env_status = ENV_RUNNING;
+		curenv->env_runs++;
+		lcr3(PADDR(curenv->env_pgdir));
+	}
 
 	env_pop_tf(&curenv->env_tf);
 
--- kern/trap.c
+++ kern/trap.c
@@ -25,6 +25,28 @@ struct Pseudodesc idt_pd = {
 	sizeof(idt) - 1, (uint32_t) idt
 };
 
+void trap_divide();
+void trap_debug();
+void trap_nmi();
+void trap_brkpt();
+void trap_oflow();
+void trap_bound();
+void trap_illop();
+void trap_device();
+void trap_dblflt();
+void trap_tss();
+void trap_segnp();
+void trap_stack();
+void trap_gpflt();
+void trap_pgflt();
+void trap_fperr();
+void trap_align();
+void trap_mchk();
+void trap_simderr();
+
+void trap_syscall();
+void trap_default();
+
 
 static const char *trapname(int trapno)
 {
@@ -65,6 +87,27 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
+	SETGATE(idt[T_DIVIDE], 0, GD_KT, trap_divide, 0);
+	SETGATE(idt[T_DEBUG], 0, GD_KT, trap_debug, 0);
+	SETGATE(idt[T_NMI], 0, GD_KT, trap_nmi, 0);
+	SETGATE(idt[T_BRKPT], 0, GD_KT, trap_brkpt, 0);
+	SETGATE(idt[T_OFLOW], 0, GD_KT, trap_oflow, 0);
+	SETGATE(idt[T_BOUND], 0, GD_KT, trap_bound, 0);
+	SETGATE(idt[T_ILLOP], 0, GD_KT, trap_illop, 0);
+	SETGATE(idt[T_DEVICE], 0, GD_KT, trap_device, 0);
+	SETGATE(idt[T_DBLFLT], 0, GD_KT, trap_dblflt, 0);
+	SETGATE(idt[T_TSS], 0, GD_KT, trap_tss, 0);
+	SETGATE(idt[T_SEGNP], 0, GD_KT, trap_segnp, 0);
+	SETGATE(idt[T_STACK], 0, GD_KT, trap_stack, 0);
+	SETGATE(idt[T_GPFLT], 0, GD_KT, trap_gpflt, 0);
+	SETGATE(idt[T_PGFLT], 0, GD_KT, trap_pgflt, 0);
+	SETGATE(idt[T_FPERR], 0, GD_KT, trap_fperr, 0);
+	SETGATE(idt[T_ALIGN], 0, GD_KT, trap_align, 0);
+	SETGATE(idt[T_MCHK], 0, GD_KT, trap_mchk, 0);
+	SETGATE(idt[T_SIMDERR], 0, GD_KT, trap_simderr, 0);
+
+	SETGATE(idt[T_SYSCALL], 0, GD_KT, trap_syscall, 3);
+	SETGATE(idt[T_DEFAULT], 0, GD_KT, trap_default, 3);
 
 	// Per-CPU setup 
 	trap_init_percpu();
